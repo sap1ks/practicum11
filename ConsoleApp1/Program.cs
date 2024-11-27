07cc61437ef9d9c69f0891c4d4a667e85dffbbab
sap1ks <sap1kcsgo@gmail.com>
2024-11-27 22:00:03 +0200

    Create Program.cs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = Input("Кол-во часов");
+            int m = Input("Кол-во минут");
+            int s = Input("Кол-во секунд");
+            int seconds = Seconds(h, m, s);
+            Output(seconds);
+            Console.ReadKey();
+        }
+        static int Seconds(int h,int m,int s)
+        {
+            return h * 3600 + m * 60 + s;
+        } 
+        static int Input(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Output(int seconds)
+        {
+            Console.WriteLine($"Всего секунд:{seconds}");
+        }
+    }
+}
